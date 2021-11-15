241f0996fa7b0b90058f2e8a1aea8fb290ea44ce
maratmuginov <mmuginov@protonmail.com>
2021-11-14 16:04:51 -0800

    Setup project structure

---
+++ source/DiscordGifHub/DiscordGifHub.Presentation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DiscordGifHub.Presentation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
